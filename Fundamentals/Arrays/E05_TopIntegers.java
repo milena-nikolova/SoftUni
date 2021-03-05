0f62a5f4d208f9f741e0f4c8a5f68e87971e2204
Milena Nikolova <49957593+milena-nikolova@users.noreply.github.com>
2021-03-05 14:43:19 +0200

    Add files via upload

---
+++ Fundamentals/Arrays/E05_TopIntegers.java
@@ -0,0 +1,29 @@
+package _03_arrays_E05_TopIntegers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class E05_TopIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            boolean isBigger = true;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] <= arr[j]) {
+                    isBigger = false;
+                    break;
+                }
+
+            }
+            if (isBigger) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
