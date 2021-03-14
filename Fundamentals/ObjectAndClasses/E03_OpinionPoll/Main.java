8fce7016167ffa104ffc259f74c4328cdcd37392
Milena Nikolova <49957593+milena-nikolova@users.noreply.github.com>
2021-03-14 21:40:13 +0200

    Add files via upload

---
+++ Fundamentals/ObjectAndClasses/E03_OpinionPoll/Main.java
@@ -0,0 +1,27 @@
+package _06_objectAndClasses_E03_OpinionPoll;
+//createdByBibiloto11112020
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Person> personsList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] data = scanner.nextLine().split("\\s+");
+            String name = data[0];
+            int age = Integer.parseInt(data[1]);
+
+            Person person = new Person(name, age);
+            personsList.add(person);
+        }
+        personsList.stream()
+                .filter(p -> p.getAge() > 30)
+                .sorted((p1, p2) -> p1.getName().compareTo(p2.getName()))
+                .forEach(p -> System.out.println(p.toString()));
+    }
+}
---
+++ Fundamentals/ObjectAndClasses/E03_OpinionPoll/Person.java
@@ -0,0 +1,24 @@
+package _06_objectAndClasses_E03_OpinionPoll;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %d", getName(), getAge());
+    }
+}
